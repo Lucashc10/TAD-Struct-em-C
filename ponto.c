8c441f8ef4698b9848e4b87bfb21decb58fc85c8
Lucas Henrique Capati <161030534+Lucashc10@users.noreply.github.com>
2025-09-19 12:28:16 -0300

    Update ponto.c

--- ponto.c
+++ ponto.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+//Incluindo o ponto.h
 #include "ponto.h"
-
+//Implementando a struct
 struct ponto {
 	int x;
 };
-
+//Implementando os métodos
 void capturaNumero(Ponto *p) {
 	printf("digite um numero \n");
+	//fazer p -> é a mesma coisa que fizesse um p.x(caso não fosse ponteiro)
 	scanf("%d", &(p -> x));
 }
 void imprimeNumero(Ponto *p) {
